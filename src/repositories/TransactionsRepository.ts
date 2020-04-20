c53b9b4e9f539b1448d12b3f8b34625c5aafcbac
gacl97 <gacl@ic.ufal.br>
2020-04-20 16:12:35 -0300

    Almost Finish

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,26 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance = transactions.reduce(
+      (result, { value, type }) => {
+        if (type === 'income') {
+          result.income += value;
+        } else {
+          result.outcome += value;
+        }
+
+        result.total = result.income - result.outcome;
+        return result;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    return balance;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  id: string;
+}
+
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: RequestDTO): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionRepository);
+
+    const transactionExists = await transactionsRepository.find({
+      where: { id },
+    });
+
+    if (!transactionExists) {
+      throw new Error('Transaction do not exists.');
+    }
+
+    await transactionsRepository.delete(id);
   }
 }
 
